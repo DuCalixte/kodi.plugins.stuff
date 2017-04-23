bbefc694e2c5d6fa6647f4304aad07a441e2d7a9
Stanley Calixte <DuCalixte@gmail.com>
2017-04-23 12:57:51 -0400

    pause for research

--- plugin.video.haiti.metro.news/addon.py
+++ plugin.video.haiti.metro.news/addon.py
@@ -1,6 +1,8 @@
 # os and lib modules
 import os
 import sys
+import re
+import string
 import urllib
 import urllib2
 
@@ -26,12 +28,36 @@ __version__ = __addon__.getAddonInfo("version")
 ICON_PATH = os.path.join(__addonDir__, "icon.png")
 FANART_PATH = os.path.join(__addonDir__, "fanart.jpg")
 
-YOUTUBE_CHANEL_URL = "https://www.youtube.com/channel/UC8rH_LswworcG_PAQGJE6jw"
+# YOUTUBE_CHANNEL_URL = "https://www.youtube.com/channel/UC8rH_LswworcG_PAQGJE6jw"
+YOUTUBE_CHANNEL_ID = "UU8rH_LswworcG_PAQGJE6jw"
+YOUTUBE_CHANNEL_URL = "https://www.youtube.com/user/pluralsight/videos"
 YOUTUBE_PLUGIN = "plugin://plugin.video.youtube/?action=play_video&videoid=%s"
 
 
 class MetroNews:
-    print "hello"
+    """
+    main class
+    """
+    def __init__(self, *args, **kwargs):
+        print "<==========================>"
+        print "Haiti MetroNews version - %s",%__version__
+        print "<==========================>"
+        self.set_debug_mode()
+        self.params = self.get_params()
+        self.mode = None
+        self.icon_image = ''
+        self.name = None
+        self.url = None
+
+    def show_contents(self):
+        response = urllib2('https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.playlistItems.list?part=snippet&maxResults=25&playlistId=UU8rH_LswworcG_PAQGJE6jw&_h=9&')
+        tree = json.loads(response.read())
+        feed = tree['feed']
+    def set_debug_mode(self):
+        self.debug_mode = False
+        if __addon__.getSetting('debug') == 'true':
+            self.debug_mode = True
+        print "MetroNews addon: debug mode: %s"%self.debug_mode
 
 ##########################################################################
 # BEGIN !
